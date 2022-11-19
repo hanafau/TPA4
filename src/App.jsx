8619a500f4b6783daad1636080e42ba7efdde7fb
Hana Fauziah <102002056+hanafau@users.noreply.github.com>
2022-11-19 16:26:59 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./components/Home/Home";
 import About from "./components/Home/About";
