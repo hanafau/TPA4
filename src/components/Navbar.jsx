3a2bc5a004176e858cef027da9a3a97283eed2b7
Hana Fauziah <102002056+hanafau@users.noreply.github.com>
2022-11-19 16:26:16 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import "../components/Navbar.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -6,17 +7,23 @@ function Navbar() {
       <div className="sub-navbar">
         <h2>Ha</h2>
         <ul>
-          <a href="/">
-            <li>Home</li>
+          <a>
+            <Link to={"/"}>
+              <li>Home</li>
+            </Link>
           </a>
-          <a href="#about">
-            <li>About</li>
+          <a>
+            <Link to={"/About"}>
+              <li>About</li>
+            </Link>
           </a>
           <a href="#portfolio">
             <li>Portfolio</li>
           </a>
-          <a href="/blog">
-            <li>Blog</li>
+          <a>
+            <Link to={"/Blog"}>
+              <li>Blog</li>
+            </Link>
           </a>
         </ul>
       </div>
